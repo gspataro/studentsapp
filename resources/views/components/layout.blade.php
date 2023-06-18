87c617e4b0e67e215e887fde898cd28060411998
Giuseppe Spataro <spatyx@gmail.com>
2023-06-18 12:16:36 +0200

    new: add tailwindcss

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>{{isset($title) ? $title . ' - StudentsApp' : 'StudentsApp'}}</title>
+        @vite('resources/css/app.css')
     </head>
 
     <body>
