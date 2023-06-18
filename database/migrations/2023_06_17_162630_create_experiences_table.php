133b63640cbd5c924fdddd30f5c76eb646bc5ea2
Giuseppe Spataro <spatyx@gmail.com>
2023-06-18 12:01:25 +0200

    new(migrations): create students, experiences and institutes tables

---
+++ database/migrations/2023_06_17_162630_create_experiences_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('experiences', function (Blueprint $table) {
+            $table->id();
+            $table->integer('student');
+            $table->string('name');
+            $table->integer('institute');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->string('mark');
+            $table->longText('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('experiences');
+    }
+};
