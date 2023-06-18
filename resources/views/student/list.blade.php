bb002f5592e450e128d1d725229629aa951634db
Giuseppe Spataro <spatyx@gmail.com>
2023-06-18 17:39:22 +0200

    update(student.list): implement new actions functionality

--- resources/views/student/list.blade.php
+++ resources/views/student/list.blade.php
@@ -1,7 +1,13 @@
 <x-layout>
-    <x-slot:title>
-        Students List
-    </x-slot:title>
+    <x-slot:title>Students List</x-slot>
+    <x-slot:actions>
+        <x-actions :items="[
+            [
+                'label' => 'New Student',
+                'url' => route('student.new')
+            ]
+        ]"/>
+    </x-slot>
 
     Hello world!
 </x-layout>
