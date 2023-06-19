dee4a20e2f137600b8f25d782320e490f41cf835
Giuseppe Spataro <spatyx@gmail.com>
2023-06-19 18:57:23 +0200

    update(StudentController): change update method and add  change method
    Change update method to make use of controller/model relationship, add change method for student update form validation

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -40,12 +40,31 @@ class StudentController extends Controller
         return to_route('student.new')->with('success', 'Student created successfully!');
     }
 
-    public function update($id): View
+    public function update(Student $student): View
     {
-        $student = Student::firstWhere('id', $id);
-
         return view('student.update', [
             'student' => $student
         ]);
+    }
+
+    public function change(Request $request, Student $student): RedirectResponse
+    {
+        $request->validate([
+            'name' => 'required|max:255',
+            'surname' => 'required|max:255',
+            'city' => 'required|max:255',
+            'birthday' => 'required|date'
+        ]);
+
+        $student->name = $request->input('name');
+        $student->surname = $request->input('surname');
+        $student->city = $request->input('city');
+        $student->birthday = $request->input('birthday');
+
+        $student->save();
+
+        return to_route('student.update', [
+            'student' => $student
+        ])->with('success', 'Student updated successfully!');
     }
 }
