e02cbbbaa2707781378708e52b7785df27974dcf
Giuseppe Spataro <spatyx@gmail.com>
2023-06-19 20:16:34 +0200

    update(view.actions): improve actions style

--- resources/views/components/actions.blade.php
+++ resources/views/components/actions.blade.php
@@ -1,7 +1,7 @@
 <ul class="flex gap-3">
     @foreach ($items as $item)
         <li>
-            <a href="{{$item['url']}}" class="uppercase font-semibold">
+            <a href="{{$item['url']}}" class="border-2 border-gray-300 rounded-lg py-3 px-5 uppercase font-semibold">
                 {{$item['label']}}
             </a>
         </li>
