53c45a9bd5bbcc8769f1a67a7447d91a468d6021
Giuseppe Spataro <spatyx@gmail.com>
2023-06-19 20:01:58 +0200

    update(view.inputs): add possibility to extend classes

--- resources/views/components/input/button.blade.php
+++ resources/views/components/input/button.blade.php
@@ -1,7 +1,6 @@
 <button
-    class="bg-indigo-600 py-2 px-3 rounded-lg text-white uppercase font-semibold"
+    class="bg-indigo-600 py-2 px-3 rounded-lg text-white uppercase font-semibold {{$class ?? ''}}"
     @isset($type)type="{{$type}}"@endisset
-    @isset($name)name="{{$name}}"@endisset
-    @isset($classes)class="{{$classes}}"@endisset>
+    @isset($name)name="{{$name}}"@endisset>
     {{$label}}
 </button>
--- resources/views/components/input/text.blade.php
+++ resources/views/components/input/text.blade.php
@@ -1,5 +1,5 @@
 <input type="text"
-    class="bg-white border-[1px] border-gray-200 rounded-lg py-2 px-3"
+    class="bg-white border-[1px] border-gray-200 rounded-lg py-2 px-3 {{$class ?? ''}}"
     @isset($name)name="{{$name}}"@endisset
     @isset($placeholder)placeholder="{{$placeholder}}"@endisset
     @isset($value)value="{{$value}}"@endisset>
