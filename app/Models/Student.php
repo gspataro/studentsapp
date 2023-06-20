e89931adfab3b2569ecbfeebffc15da19c016b10
Giuseppe Spataro <spatyx@gmail.com>
2023-06-20 17:31:07 +0200

    update(model.student): add relation with institutes

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Student extends Model
 {
     use HasFactory;
+
+    /**
+     * Get student institute
+     *
+     * @return HasOne
+     */
+
+    public function institute(): HasOne
+    {
+        return $this->hasOne(Institute::class, 'id', 'institute_id');
+    }
 }
