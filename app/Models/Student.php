07ad134ba22aaa3f7afa2f4043b95bd51c4407a1
Giuseppe Spataro <spatyx@gmail.com>
2023-06-21 21:05:26 +0200

    update(model.student): add relationship with grades

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Student extends Model
@@ -19,5 +20,16 @@ class Student extends Model
     public function institute(): HasOne
     {
         return $this->hasOne(Institute::class, 'id', 'institute_id');
+    }
+
+    /**
+     * Get student's grades
+     *
+     * @return HasMany
+     */
+
+    public function grades(): HasMany
+    {
+        return $this->hasMany(Grade::class);
     }
 }
