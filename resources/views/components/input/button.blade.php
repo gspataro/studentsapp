758b485201093ed0ad212c9054512dd50f850794
Giuseppe Spataro <spatyx@gmail.com>
2023-06-22 20:35:35 +0200

    update(view.input.button): add support for attributes merge

--- resources/views/components/input/button.blade.php
+++ resources/views/components/input/button.blade.php
@@ -1,6 +1,6 @@
 <button
-    class="bg-indigo-600 py-2 px-3 rounded-lg text-white uppercase font-semibold {{$class ?? ''}}"
     @isset($type)type="{{$type}}"@endisset
-    @isset($name)name="{{$name}}"@endisset>
+    @isset($name)name="{{$name}}"@endisset
+    {{$attributes->merge(['class' => 'bg-indigo-600 py-2 px-3 rounded-lg text-white uppercase font-semibold'])}}>
     {{$label}}
 </button>
