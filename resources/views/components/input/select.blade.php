a746b4bf0ac0b799aaeb748537bb66c37845ddf4
Giuseppe Spataro <spatyx@gmail.com>
2023-06-22 19:30:12 +0200

    refactor(view.input.select): make code more compact

--- resources/views/components/input/select.blade.php
+++ resources/views/components/input/select.blade.php
@@ -1,8 +1,7 @@
 <select name="{{$name}}" class="bg-white border-[1px] border-gray-200 rounded-lg py-2 px-3 {{$class ?? ''}}">
     @if (isset($items))
         @foreach ($items as $item)
-            @php($itemSelected = isset($selected) && $item['value'] == $selected)
-            <option value="{{$item['value']}}" @if ($itemSelected) selected @endif>{{$item['label']}}</option>
+            <option value="{{$item['value']}}" {{isset($selected) && $item['value'] == $selected ? 'selected' : ''}}>{{$item['label']}}</option>
         @endforeach
     @endif
 </select>
