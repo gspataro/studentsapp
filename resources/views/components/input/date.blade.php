5b226e3a29433b477a371e64c9ee13179eedc15e
Giuseppe Spataro <spatyx@gmail.com>
2023-06-22 19:39:26 +0200

    update(view.input): make use of attributes merge

--- resources/views/components/input/date.blade.php
+++ resources/views/components/input/date.blade.php
@@ -1,4 +1,4 @@
 <input type="date"
-    class="bg-white border-[1px] border-gray-200 rounded-lg py-2 px-3 {{$class ?? ''}}"
     @isset($name)name="{{$name}}"@endisset
-    @isset($value)value="{{$value}}"@endisset>
+    @isset($value)value="{{$value}}"@endisset
+    {{$attributes->merge(['class' => 'bg-white border border-gray-200 rounded-lg py-2 px-3'])}}>
