a2e743d429c63a6fca2a1926ff4f0fddce0e8fe0
Giuseppe Spataro <spatyx@gmail.com>
2023-07-04 15:30:16 +0200

    update(view.livewire.grade.edit): improve input style

--- resources/views/livewire/grade/edit.blade.php
+++ resources/views/livewire/grade/edit.blade.php
@@ -2,15 +2,15 @@
     @if ($editMode)
         <form class="contents" wire:submit.prevent="submit">
             <div class="p-2 border-b border-b-gray-200 text-left">
-                <x-input.text class="py-1 px-1" wire:model="grade.subject" />
+                <x-input.text class="py-1 px-0 border-0 border-b rounded-none outline-none focus:border-gray-400" wire:model="grade.subject" />
                 @error('grade.subject') <div class="text-red-600">{{$message}}</div> @enderror
             </div>
             <div class="p-2 border-b border-b-gray-200 text-left">
-                <x-input.text class="py-1 px-1" wire:model="grade.grade" />
+                <x-input.text class="py-1 px-0 border-0 border-b rounded-none outline-none focus:border-gray-400" wire:model="grade.grade" />
                 @error('grade.grade') <div class="text-red-600">{{$message}}</div> @enderror
             </div>
             <div class="p-2 border-b border-b-gray-200 text-left">
-                <x-input.date class="py-1 px-1" wire:model="grade.date" />
+                <x-input.date class="py-1 px-0 border-0 border-b rounded-none outline-none focus:border-gray-400" wire:model="grade.date" />
                 @error('grade.date') <div class="text-red-600">{{$message}}</div> @enderror
             </div>
             <ul class="flex gap-3 items-center p-2 border-b border-b-gray-200 text-left">
